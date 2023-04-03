d2d05462c4f8314981ebcd9e79e8486a1b298e24
diegodelc <diegodelcastillo2001@gmail.com>
2023-04-03 18:17:14 +0100

    adding untracked files

---
+++ jax_cfd/ml/diego_towers.py
@@ -0,0 +1,38 @@
+import haiku as hk
+from jax_cfd.ml import nonlinearities
+
+class CNN(hk.Module):
+    def __init__(self,CNN_specs=None):
+        if CNN_specs == None:
+            CNN_specs = {
+                "hidden_channels" : 16,
+                "hidden_layers" : 2,
+                "nonlinearity" : "relu",
+                "num_output_channels" : 8
+            }
+        super().__init__(name="CNN")
+        components = []
+        
+        if CNN_specs["nonlinearity"] == "relu":
+            nonlinearity = nonlinearities.relu
+        
+        for i in range(CNN_specs["hidden_layers"]):
+            components.append(hk.Conv2D(output_channels=CNN_specs["hidden_channels"], kernel_shape=(3,3), padding="SAME"))
+            components.append(nonlinearity)
+        
+        components.append(hk.Conv2D(output_channels=CNN_specs["num_output_channels"], kernel_shape=(3,3), padding="SAME"))
+        
+        self.components = components
+        
+        
+    def __call__(self, x):
+        x = hk.Sequential(self.components)(x)
+        return x
+
+# def ConvNet(x,CNN_specs=None):
+#     cnn = CNN(CNN_specs)
+#     return cnn(x)
+
+
+def build_forward_pass(CNN_specs=None):
+    return hk.without_apply_rng(hk.transform(ConvNet))
---
+++ jax_cfd/ml/newSaveObject.py
@@ -0,0 +1,6 @@
+import haiku as hk
+
+class newSaveObject():
+    def __init__(self,params,CNN_specs=None):
+        self.params = params
+        self.CNN_specs = CNN_specs
